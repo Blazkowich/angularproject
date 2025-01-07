120507cdcbbc9283911949547f6d795c707f03f2
Blazkowich <oiluridze@gmail.com>
2025-01-07 22:41:45 +0400

    login page fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MainPageComponent } from "./commander/main-page/main-page.component";
+import { CreateJobComponent } from "./commander/create-job/create-job.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainPageComponent],
+  imports: [MainPageComponent, CreateJobComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
