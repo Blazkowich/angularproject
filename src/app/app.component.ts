24c68e22bf0b225dfde545d5d84a026557bd1f06
Blazkowich <oiluridze@gmail.com>
2025-01-07 23:03:33 +0400

    added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MainPageComponent } from "./commander/main-page/main-page.component";
 import { CreateJobComponent } from "./commander/create-job/create-job.component";
 import { LoginComponent } from "./login/login.component";
+import { QuestionAnswerComponent } from "./question-answer/question-answer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainPageComponent, CreateJobComponent, LoginComponent],
+  imports: [MainPageComponent, CreateJobComponent, LoginComponent, QuestionAnswerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
