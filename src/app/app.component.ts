34e82b0f4cee811f4b74feb5105ba6214080037f
Blazkowich <oiluridze@gmail.com>
2025-01-08 12:52:26 +0400

    added many things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LoginComponent } from "./login/login.component";
 import { QuestionAnswerComponent } from "./question-answer/question-answer.component";
 import { ControlIconsComponent } from "./shared/control-icons/control-icons.component";
 import { ImageComponent } from "./shared/image/image.component";
+import { JobDetailsComponent } from "./commander/job-details/job-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainPageComponent, CreateJobComponent, LoginComponent, QuestionAnswerComponent, ControlIconsComponent, ImageComponent],
+  imports: [MainPageComponent, CreateJobComponent, LoginComponent, QuestionAnswerComponent, ControlIconsComponent, ImageComponent, JobDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
