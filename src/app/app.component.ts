3bb334674445cb9fa18c6a8a32c064e820a0b234
Blazkowich <oiluridze@gmail.com>
2025-01-08 11:07:40 +0400

    rewrite control icons on bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { MainPageComponent } from "./commander/main-page/main-page.component";
 import { CreateJobComponent } from "./commander/create-job/create-job.component";
 import { LoginComponent } from "./login/login.component";
 import { QuestionAnswerComponent } from "./question-answer/question-answer.component";
+import { ControlIconsComponent } from "./shared/control-icons/control-icons.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainPageComponent, CreateJobComponent, LoginComponent, QuestionAnswerComponent],
+  imports: [MainPageComponent, CreateJobComponent, LoginComponent, QuestionAnswerComponent, ControlIconsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
