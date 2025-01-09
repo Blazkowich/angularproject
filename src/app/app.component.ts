04cededfee03f7c403a97e053665ab709dd9fcf8
Blazkowich <oiluridze@gmail.com>
2025-01-09 12:19:25 +0400

    added candidate profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CandidateProfileComponent } from "./commander/candidate-profile/candidate-profile.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CandidateProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
