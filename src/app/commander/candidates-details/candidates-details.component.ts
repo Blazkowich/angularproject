f9fa7faf2d3754266ad73fa80da9d6fffd5f051b
Blazkowich <oiluridze@gmail.com>
2025-01-09 09:04:51 +0400

    added chevron to candidates box

---
+++ src/app/commander/candidates-details/candidates-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidates-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './candidates-details.component.html',
+  styleUrl: './candidates-details.component.css'
+})
+export class CandidatesDetailsComponent {
+
+}
