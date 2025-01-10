2f4df0417b157776761cd7bea098d1c1fde3253d
Blazkowich <oiluridze@gmail.com>
2025-01-10 21:48:18 +0400

    aded job application details page

--- src/app/user/job-application-details/job-application-details.component.ts
+++ src/app/user/job-application-details/job-application-details.component.ts
@@ -1,15 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
 import { ApplyVacancyPopupComponent } from '../../shared/popup-windows/apply-vacancy-popup/apply-vacancy-popup.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-job-application-details',
   standalone: true,
-  imports: [ApplyVacancyPopupComponent],
+  imports: [ApplyVacancyPopupComponent, CommonModule],
   templateUrl: './job-application-details.component.html',
   styleUrl: './job-application-details.component.css'
 })
 export class JobApplicationDetailsComponent {
   @ViewChild(ApplyVacancyPopupComponent) popup!: ApplyVacancyPopupComponent;
+  activeTab: 'jobRequirements' | 'faq' = 'jobRequirements';
+
+  switchTab(tab: 'jobRequirements' | 'faq') {
+    this.activeTab = tab;
+  }
 
   openPopup() {
     this.popup.show();
