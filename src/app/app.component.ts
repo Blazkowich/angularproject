7907df02eb2fd35d595489da6171f38d120ebfd4
Blazkowich <oiluridze@gmail.com>
2025-01-11 11:58:40 +0400

    refactor user main page component and update layout styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CandidateProfileComponent } from "./commander/candidate/candidate-profile/candidate-profile.component";
-import { InterviewSummaryComponent } from "./commander/interview-summary/interview-summary.component";
-import { UserMainPageComponent } from "./user/user-main-page/user-main-page.component";
-import { JobApplicationDetailsComponent } from "./user/job-application-details/job-application-details.component";
 import { UserProfileComponent } from "./user/user-profile/user-profile.component";
+import { UserMainPageComponent } from "./user/user-main-page/user-main-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CandidateProfileComponent, InterviewSummaryComponent, UserMainPageComponent, JobApplicationDetailsComponent, UserProfileComponent],
+  imports: [RouterModule, UserMainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
