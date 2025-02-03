a5fa29674a33e22534af0ab05a168205d53c49ad
anz-davar <anzori94@gmail.com>
2025-02-03 17:29:23 +0200

    try hashing routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -7,7 +8,8 @@ import { authInterceptor } from './shared/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), // Add withHashLocation()
+    // provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(
       withFetch(),
